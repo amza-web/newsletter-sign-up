a94326f6935fd96bdd04257cbc005e78a5869130
Amza <amza.web@gmail.com>
2023-06-13 23:38:00 +0100

    added a function for the dismiss btn

--- components/Success.js
+++ components/Success.js
@@ -2,7 +2,11 @@ import React from 'react'
 import img3 from '@/app/icon-success.svg'
 import Image from 'next/image'
 
-const Success = ({email}) => {
+const Success = ({email, setFinish, setEmail}) => {
+  function handler(e){
+    setEmail("")
+    setFinish(false);
+  }
   return (
     <div className='font-roboto font-medium text-lg lg:flex lg:place-content-center lg:place-items-center lg:min-h-screen'>
         <div className='p-10 md:p-16 lg:bg-white lg:rounded-3xl lg:flex lg:p-0'>
@@ -11,7 +15,7 @@ const Success = ({email}) => {
                 <Image className='pb-4' src={img3} alt='checkmark'/>
                 <h1 className='text-4xl md:text-5xl font-bold lg:text-6xl'>Thanks for subscribing!</h1>
                 <p className=''>A confirmation email has been sent to <span className='font-bold'>{email}</span>. Please open it and click the button inside to confirm your subscription.</p>
-                <input className='inp font-medium' type='submit' value='Dismiss Message'/>
+                <input className='inp font-medium' type='submit' value='Dismiss Message' onClick={handler}/>
             </div>
             </div>
         </div>
